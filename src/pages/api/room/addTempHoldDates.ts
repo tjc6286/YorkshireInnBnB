63af4c68f986b748e4b4b667ef9c1674dff66c81
Francisco Paliouras <franciscopaliouras@gmail.com>
2023-03-22 12:58:55 -0400

    added addTempHold endpoint

--- src/pages/api/room/addTempHoldDates.ts
+++ src/pages/api/room/addTempHoldDates.ts
@@ -1,5 +1,5 @@
-import { getRoomById } from "../../../lib/rooms";
 import type { APIRoute } from "astro";
+import { addHoldDates } from "../../../lib/rooms.js";
 
 /**
  *
@@ -7,11 +7,12 @@ import type { APIRoute } from "astro";
  */
 export const post: APIRoute = async ({ request }) => {
   var data = await request.json();
-  
   if (request.headers.get("Content-Type") === "application/json") {
-    const id = data.id;
+    
+    const roomId = data.roomId;
+    const dates = data.dates;
     //VALIDATION FOR DATES, if NO DATES return ERROR MESSAGE
-    const room = await getRoomById(id);
+    const room = await addHoldDates(roomId, dates);
     return new Response(JSON.stringify(room), {
       status: 200,
     });
