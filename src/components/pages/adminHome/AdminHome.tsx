36bb83e943868a9dd0e7bbcb741d727f60abc2de
Francisco Paliouras <franciscopaliouras@gmail.com>
2023-04-02 18:32:11 -0400

    front end for admin room

--- src/components/pages/adminHome/AdminHome.tsx
+++ src/components/pages/adminHome/AdminHome.tsx
@@ -32,8 +32,7 @@ const AdminHome: React.FC = () => {
               <p className="text-white">Logged in as : {userEmail}</p>
               <a
                 className="bg-gray-900 text-white/50 p-2 rounded-md hover:text-white smooth-hover"
-                href="#"
-              >
+                href="#">
                 <button onClick={() => signOutUser()}>Logout</button>
               </a>
             </div>
@@ -48,21 +47,18 @@ const AdminHome: React.FC = () => {
                 stroke="currentColor"
                 strokeWidth="1.5"
                 viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+                xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
                   strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
+                  strokeLinejoin="round"></path>
               </svg>
               <h4 className="text-white text-2xl font-bold capitalize text-center"></h4>
               <p className="text-white/50">Manage bookings</p>
             </div>
             <a
               href="/adminRoom"
-              className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover"
-            >
+              className="relative group bg-gray-900 py-10 sm:py-20 px-4 flex flex-col space-y-2 items-center cursor-pointer rounded-md hover:bg-gray-900/80 hover:smooth-hover">
               <svg
                 className="text-white w-[30%]"
                 aria-hidden="true"
@@ -70,13 +66,11 @@ const AdminHome: React.FC = () => {
                 stroke="currentColor"
                 strokeWidth="1.5"
                 viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+                xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
+                  strokeLinejoin="round"></path>
               </svg>
               <h4 className="text-white text-2xl font-bold capitalize text-center"></h4>
               <p className="text-white/50 text-center">
@@ -91,13 +85,11 @@ const AdminHome: React.FC = () => {
                 stroke="currentColor"
                 strokeWidth="1.5"
                 viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+                xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
                   strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
+                  strokeLinejoin="round"></path>
               </svg>
               <h4 className="text-white text-2xl font-bold capitalize text-center"></h4>
               <p className="text-white/50 text-center">Generate Reports</p>
@@ -110,13 +102,11 @@ const AdminHome: React.FC = () => {
                 stroke="currentColor"
                 strokeWidth="1.5"
                 viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+                xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
                   strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
+                  strokeLinejoin="round"></path>
               </svg>
               <h4 className="text-white text-2xl font-bold capitalize text-center"></h4>
               <p className="text-white/50 text-center">Manage Reviews</p>
--- src/components/pages/adminRoom/AdminRoom.tsx
+++ src/components/pages/adminRoom/AdminRoom.tsx
@@ -1,13 +1,37 @@
+import { Checkbox, FormControlLabel, Radio, RadioGroup } from "@mui/material";
+import TextField from "@mui/material/TextField";
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { addDays } from "date-fns";
-import React from "react";
+import { addDays, format } from "date-fns";
+import React, { useEffect } from "react";
 import { auth, signOutUser } from "../../../firebase";
+import type SpecialDatePrice from "../../../types/specialDatePrice";
 
 const AdminRoom: React.FC = () => {
   const [userEmail, setUserEmail] = React.useState("");
   const [startDate, setStartDate] = React.useState<string | null>(null);
   const [endDate, setEndDate] = React.useState<string | null>(null);
+  const [updatePrice, setUpdatePrice] = React.useState<string>("");
+  const [isBlocking, setIsBlocking] = React.useState(false);
+  const [selectedRoom, setSelectedRoom] = React.useState<string>("");
+  const [roomList, setRoomList] = React.useState<any[]>([]);
+  const [blockedMap, setBlockMap] = React.useState<any[]>([]);
+  const [priceMap, setPriceMap] = React.useState<any[]>([]);
+
+  const handlePriceChange = (event: any) => {
+    setUpdatePrice(event.target.value);
+  };
+
+  const handleCheckboxChange = (event: any) => {
+    setIsBlocking(event.target.checked);
+    if (event.target.checked) {
+      setUpdatePrice("");
+    }
+  };
+
+  const handleRoomSelect = (event: any) => {
+    setSelectedRoom(event.target.value);
+  };
 
   auth.onAuthStateChanged((user) => {
     if (user) {
@@ -44,6 +68,65 @@ const AdminRoom: React.FC = () => {
     setEndDate(null);
   };
 
+  const handleSubmit = () => {
+    if (startDate && endDate) {
+      if (isBlocking || updatePrice !== "") {
+        console.log("submitting");
+        console.log(startDate);
+        console.log(endDate);
+        console.log(updatePrice);
+        console.log(isBlocking);
+        console.log(selectedRoom);
+        // Reset all fields
+        resetDates();
+        setIsBlocking(false);
+        setUpdatePrice("");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetch("/api/room/getAll", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setRoomList(data);
+        // Create a map of blocked rooms
+        const blockedMap: Array<any> = [];
+
+        // Create a map of blocked rooms
+        const priceMap: Array<any> = [];
+
+        //getting the blocked dates for each room
+        data.forEach((room: any) => {
+          room.unavailableDates.forEach((date: any) => {
+            blockedMap.push({
+              key: room.name + date,
+              room: room.name,
+              date: date,
+            });
+          });
+
+          room.specialPriceDates.forEach(
+            (specialPriceDates: SpecialDatePrice) => {
+              priceMap.push({
+                key: room.name + specialPriceDates.date,
+                room: room.name,
+                date: specialPriceDates.date,
+                price: specialPriceDates.price,
+              });
+            }
+          );
+        });
+        setPriceMap(priceMap);
+        setBlockMap(blockedMap);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
     <div className="bg-gray-900 min-h-screen flex items-center justify-center">
       <div className="bg-gray-800 flex-1 flex flex-col space-y-5 lg:space-y-0 lg:flex-row lg:space-x-10 max-w-6xl sm:p-6 sm:my-2 sm:mx-4 sm:rounded-2xl">
@@ -52,8 +135,7 @@ const AdminRoom: React.FC = () => {
           <div className="flex justify-between items-center">
             <a
               href="/adminHome"
-              className="bg-gray-900 text-white/50 p-2 rounded-md hover:text-white smooth-hover"
-            >
+              className="bg-gray-900 text-white/50 p-2 rounded-md hover:text-white smooth-hover">
               {" "}
               Return Home
             </a>
@@ -64,60 +146,183 @@ const AdminRoom: React.FC = () => {
               <p className="text-white">Logged in as : {userEmail}</p>
               <a
                 className="bg-gray-900 text-white/50 p-2 rounded-md hover:text-white smooth-hover"
-                href="#"
-              >
+                href="#">
                 <button onClick={() => signOutUser()}>Logout</button>
               </a>
             </div>
           </div>
           {/* Content */}
-          <div className="h-[50%] pt-10">
-            <div className="mx-4">
-              <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DatePicker
-                  label={"Start Date"}
-                  value={startDate ? new Date(startDate) : null}
-                  disablePast
-                  maxDate={addDays(new Date(), 365)}
-                  onChange={(newValue: Date | null) => {
-                    newValue && handleSetStartDate(newValue.toString());
-                  }}
-                  componentsProps={{ textField: { variant: "outlined" } }}
-                />
-              </LocalizationProvider>
-            </div>
-            <div className="mx-4">
-              <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DatePicker
-                  label={"End Date"}
-                  value={endDate ? new Date(endDate) : null}
-                  disablePast
-                  maxDate={addDays(new Date(), 365)}
-                  minDate={startDate ? new Date(startDate) : undefined}
-                  disabled={!startDate}
-                  onChange={(newValue: Date | null) => {
-                    if (newValue && newValue <= new Date()) return;
+          <div className="h-[50%] pt-10 text-white">
+            <h2 className="text-2xl font-bold mb-2">
+              Room Blocking/ Priceing Update
+            </h2>
+            {/* TOP CONTROLS */}
+            <div className="flex">
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={isBlocking}
+                    onChange={handleCheckboxChange}
+                  />
+                }
+                label="Block Selected Room"
+              />
+              <TextField
+                id="outlined-basic"
+                label="Price to Update"
+                variant="outlined"
+                value={updatePrice}
+                onChange={handlePriceChange}
+                disabled={isBlocking}
+              />
+              <div className="mx-4">
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                    label={"Start Date"}
+                    value={startDate ? new Date(startDate) : null}
+                    disablePast
+                    maxDate={addDays(new Date(), 365)}
+                    onChange={(newValue: Date | null) => {
+                      newValue && handleSetStartDate(newValue.toString());
+                    }}
+                    componentsProps={{ textField: { variant: "outlined" } }}
+                  />
+                </LocalizationProvider>
+              </div>
+              <div className="mx-4">
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                    label={"End Date"}
+                    value={endDate ? new Date(endDate) : null}
+                    disablePast
+                    maxDate={addDays(new Date(), 365)}
+                    minDate={startDate ? new Date(startDate) : undefined}
+                    disabled={!startDate}
+                    onChange={(newValue: Date | null) => {
+                      if (newValue && newValue <= new Date()) return;
 
-                    newValue && handleSetEndDate(newValue.toString());
-                  }}
-                  componentsProps={{ textField: { variant: "outlined" } }}
-                />
-              </LocalizationProvider>
+                      newValue && handleSetEndDate(newValue.toString());
+                    }}
+                    componentsProps={{ textField: { variant: "outlined" } }}
+                  />
+                </LocalizationProvider>
+              </div>
+              <button
+                disabled={!endDate}
+                className="px-5 py-2 inline-block bg-transparent outline rounded-md text-white outline-1 bg-gray-600 transition-colors mx-px"
+                style={{
+                  fontFamily: "Martel",
+                  fontWeight: "400",
+                  fontSize: "20px",
+                  lineHeight: "34px",
+                  letterSpacing: "0.13em",
+                }}
+                onClick={handleSubmit}>
+                Submit
+              </button>
             </div>
-            <button
-              disabled={!endDate}
-              className="px-5 py-2 inline-block bg-transparent outline rounded-md text-white outline-1 bg-gray-600 transition-colors mx-px"
-              style={{
-                fontFamily: "Martel",
-                fontWeight: "400",
-                fontSize: "20px",
-                lineHeight: "34px",
-                letterSpacing: "0.13em",
-              }}
-              onClick={() => {}}
-            >
-              Submit
-            </button>
+            {/* ROOM SELECTION TABLE */}
+            <RadioGroup value={selectedRoom} onChange={handleRoomSelect} row>
+              {roomList.map((room) => {
+                return (
+                  <FormControlLabel
+                    key={room._id}
+                    value={room._id}
+                    control={<Radio />}
+                    label={room.name}
+                  />
+                );
+              })}
+            </RadioGroup>
+            {/* Table for Room priceing or blocked */}
+            <h2 className="text-2xl font-bold my-2">Blocked Off Dates</h2>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      Room Name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Blocked Date Range
+                    </th>
+                    <th scope="col" className="px-6 py-3"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {blockedMap.map((entry) => {
+                    return (
+                      <tr
+                        key={entry.key}
+                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          {entry.room}
+                        </th>
+                        <td className="px-6 py-4">
+                          {format(new Date(entry.date), "MM/dd/yyyy")}
+                        </td>
+                        <td className="px-6 py-4 text-right">
+                          <a
+                            href="#"
+                            className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                            Remove Block
+                          </a>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+            <hr className="my-4" />
+            {/* TABLE FOR ROOM PRICING */}
+            <h2 className="text-2xl font-bold my-2">Special Price Dates</h2>
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      Room Name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Date Range
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Price Change
+                    </th>
+                    <th scope="col" className="px-6 py-3"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {priceMap.map((entry) => {
+                    return (
+                      <tr
+                        key={entry.key}
+                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          {entry.room}
+                        </th>
+                        <td className="px-6 py-4">
+                          {format(new Date(entry.date), "MM/dd/yyyy")}
+                        </td>
+                        <td className="px-6 py-4">$ {entry.price}</td>
+                        <td className="px-6 py-4 text-right">
+                          <a
+                            href="#"
+                            className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                            Remove Price Change
+                          </a>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
